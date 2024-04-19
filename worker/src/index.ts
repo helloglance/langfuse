4fcedb53e9269c99278f87f2258987db6e01cd3b
Agam Patel <agamrp@gmail.com>
2024-04-18 17:23:51 -0700

    just hardcode worker port to get around address in use error for deploying without docker compose

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,29 +1,28 @@
-import app from "./app";
-import { env } from "./env";
-import logger from "./logger";
-import { evalJobCreator, evalJobExecutor } from "./redis/consumer";
+import app from './app';
+import { env } from './env';
+import logger from './logger';
+import { evalJobCreator, evalJobExecutor } from './redis/consumer';
 
-const server = app.listen(env.PORT, () => {
-  logger.info(`Listening: http://localhost:${env.PORT}`);
+const server = app.listen(3030, () => {
+  logger.info(`Listening: http://localhost:${3030}`);
 });
+// const server = app.listen(env.PORT, () => {
+//   logger.info(`Listening: http://localhost:${env.PORT}`);
+// });
 
 // Function to handle shutdown logic
 function onShutdown() {
-  logger.info("Shutting down application...");
+  logger.info('Shutting down application...');
 
   // Stop accepting new connections
   server.close();
 
   // Perform necessary cleanup tasks here
   // For example, close database connections, stop job executors, etc.
-  evalJobCreator
-    ?.close()
-    .then(() => logger.info("Eval Job Creator has been closed."));
-  evalJobExecutor
-    ?.close()
-    .then(() => logger.info("Eval Job Executor has been closed."));
+  evalJobCreator?.close().then(() => logger.info('Eval Job Creator has been closed.'));
+  evalJobExecutor?.close().then(() => logger.info('Eval Job Executor has been closed.'));
 }
 
 // Capture shutdown signals
-process.on("SIGTERM", onShutdown);
-process.on("SIGINT", onShutdown);
+process.on('SIGTERM', onShutdown);
+process.on('SIGINT', onShutdown);
